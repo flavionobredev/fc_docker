5f4068515193f6cc0e15b55d7677f967aa11c7c7
Flavio Nobre <flavionobredev@gmail.com>
2022-12-20 14:58:51 -0300

    chore: add mysql service

--- nginx-nodejs/api/src/app.js
+++ nginx-nodejs/api/src/app.js
@@ -2,7 +2,7 @@ const app = require("express")();
 
 app.get("/", (req, res) => {
   res.removeHeader("X-Powered-By");
-  res.send("Hello World!");
+  res.send("<h1>Full Cycle Rocks!</h1>");
 });
 
 module.exports = {
