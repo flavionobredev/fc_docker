b616e68b0824b8cf8961a355140092976038a149
Flavio Nobre <flavionobredev@gmail.com>
2022-12-21 12:12:14 -0300

    chore: add dockerize

--- nginx-nodejs/migration.sql
+++ nginx-nodejs/migration.sql
@@ -1,6 +1,11 @@
+CREATE DATABASE IF NOT EXISTS fcdockerdb;
+CREATE USER 'fcdockerdbuser'@'%' IDENTIFIED BY 'fcdockerdbpwd';
+GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `fcdockerdb`.* TO 'fcdockerdbuser'@'%';
+FLUSH PRIVILEGES;
+
 USE fcdockerdb;
 
-DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS people;
 
 CREATE TABLE people (
   id int(11) unsigned NOT NULL AUTO_INCREMENT,
